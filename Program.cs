4f699f11178d9d90d596a1f4c421b09f0313fd19
Dipa1999840 <20020896@students.koi.edu.au>
2024-08-11 04:00:43 +0600

    create Average

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Average
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int gradecounter = 1;
+            int total = 0;
+            while (gradecounter <= 5)
+            {
+                Console.Write("Enter the grade: ");
+                int grade = Convert.ToInt32(Console.ReadLine());
+                total = total + grade;
+                gradecounter = gradecounter + 1;
+            }
+            Console.WriteLine("The average of the class is : " + total);
+        }
+
+    }
+}
+
